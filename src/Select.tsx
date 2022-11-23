561914b0d87cb5aecf77331d111ba244b02f23c9
RamyALaa <ramy.aloocy@gmail.com>
2022-11-23 16:50:39 +0000

    100%

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import styles from "./Select.module.css"
 export type SelectOption = {
     label : string
@@ -30,6 +30,8 @@ const Select = ({multiple , value, onChange, options} : SelectProps) => {
         
     }
 
+    const containerRef = useRef<HTMLDivElement>(null);
+
     const selectOption = (option : SelectOption) =>{
         if (multiple){
             if (value.includes(option))
@@ -52,9 +54,46 @@ const Select = ({multiple , value, onChange, options} : SelectProps) => {
         if(isOpen) setHighlightedIndex(0)
     }, [isOpen])
 
+    useEffect(()=>{
+       const handler = (e: KeyboardEvent) =>{
+        if (e.target != containerRef.current) return
+            switch(e.code){
+                case "Enter":
+                case "Space":
+                    setIsOpen(prev => !prev)
+                    if(isOpen) selectOption(options[highlightedIndex])
+                break
+                case "ArrowUp":
+                case "ArrowDown":{
+                    if(!isOpen) {
+                        setIsOpen(true)
+                        break
+                    }   
+                    const newValue = highlightedIndex + (e.code === "ArrowUp" ? -1 : +1)
+                    if(newValue >=0 && newValue< options.length)
+                        setHighlightedIndex(newValue)
+                    break
+                }
+                
+                case "Escape":
+                    if(isOpen) setIsOpen(false)
+                    break
+            }
+
+       } 
+
+       containerRef.current?.addEventListener('keydown', handler)
+
+       return () =>{
+        containerRef.current?.removeEventListener('keydown', handler)
+       }
+
+
+    }, [isOpen, highlightedIndex, options])
 
     return ( 
         <div 
+        ref={containerRef}
         onClick={()=> setIsOpen(prev => !prev)} 
         tabIndex={0} 
         className={styles.container}
